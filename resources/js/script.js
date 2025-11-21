133d32ecb0b6d1d45820a48c66a1df5cd09cad70
DayKunG <canthrpheythaksin@gmail.com>
2025-11-21 18:27:29 +0700

    Fix Supabase client import for Vercel build

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ymwxkvuekkjvszmlhmiu.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inltd3hrdnVla2tqdnN6bWxobWl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1MzkzMTAsImV4cCI6MjA3OTExNTMxMH0.ChHxnaEQ4e9XbdsAMatkrLpy7JH4uFcafkSfF4puOjM"; // public key ของคุณ
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inltd3hrdnVla2tqdnN6bWxobWl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1MzkzMTAsImV4cCI6MjA3OTExNTMxMH0.ChHxnaEQ4e9XbdsAMatkrLpy7JH4uFcafkSfF4puOjM' // public key ของคุณ
+const supabase = createClient(supabaseUrl, supabaseKey)
